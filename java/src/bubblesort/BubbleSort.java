e3cc28b6dc6b1256ac027b556cee5ba8876d61bb
74th <site@j74th.com>
2017-09-03 13:07:10 +0900

    format

--- java/src/bubblesort/BubbleSort.java
+++ java/src/bubblesort/BubbleSort.java
@@ -3,7 +3,7 @@ package bubblesort;
 import java.util.*;
 
 class BubbleSort {
-	 public void Sort(int[] list){
+	 public void Sort(int[] list) {
 		 for (int i = 0; i < list.length - 1; i++) {
 			for (int j = 0; j < list.length - 1 - i; j++) {
 				if (list[j] >list[j + 1]) {
--- java/src/bubblesort/BubbleSorter.java
+++ java/src/bubblesort/BubbleSorter.java
@@ -3,18 +3,18 @@ package bubblesort;
 import java.util.*;
 
 class BubbleSorter {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 
-		BubbleSort sort= new BubbleSort();
+		BubbleSort sort = new BubbleSort();
 
 		int[] list = new int[args.length];
-		for(int i=0;i<args.length;i++){
+		for(int i=0; i < args.length; i++) {
 			list[i] = Integer.parseInt(args[i]);
 		}
 		sort.Sort(list);
-		for(int i=0;i<list.length;i++){
-		System.out.print(list[i]);
-		System.out.print(" ");
+		for(int i=0; i < list.length; i++) {
+			System.out.print(list[i]);
+			System.out.print(" ");
 		}
 		System.out.println("");
 	}
