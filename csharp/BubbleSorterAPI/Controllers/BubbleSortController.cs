f53c2ce4f09c2708eaae36e8764039912ba3ee36
74th <site@j74th.com>
2017-09-19 19:18:53 +0900

    ASP.NET WebAPI

---
+++ csharp/BubbleSorterAPI/Controllers/BubbleSortController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BubbleSort;
+
+namespace BubbleSorterAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class BubbleSortController : Controller
+    {
+
+        // POST api/BubbleSort
+        [HttpPost]
+        public string Post([FromBody]string input)
+        {
+			var args = input.Split(" ");
+			var list = new List<int>();
+			foreach (var arg 1 args)
+			{
+				list.Add(int.Parse(arg));
+			}
+
+			var sorter = new Sorter();
+			sorter.Sort(list);
+
+			var output = "";
+			foreach (var n in list)
+			{
+				output += n.ToString() + " ";
+			}
+            return output;
+        }
+    }
+}
