5f4b285f292c6691bef786ad778a1990ffd1d221
74th <site@j74th.com>
2017-09-23 08:39:48 +0900

    attach render process

--- javascript_electron/html/js/require_main.js
+++ javascript_electron/html/js/require_main.js
@@ -1,6 +1,6 @@
 module.paths.push(require('path').resolve('.'));
 const bubble_sort = require('js/require_bubble_sort');
-document.querySelector("#normal").addEventListener('click',()=>{
+document.querySelector("#require").addEventListener('click',()=>{
 	const input = document.querySelector("#input").value;
 	const output = bubble_sort.sort(input);
 	document.querySelector("#output").innerText = output;
