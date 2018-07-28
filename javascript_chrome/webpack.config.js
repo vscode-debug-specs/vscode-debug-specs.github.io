43cb254571fc572e884b8c878cb9eaa5951ed20a
74th <site@j74th.com>
2018-07-29 08:13:47 +0900

    update latest

--- javascript_chrome/webpack.config.js
+++ javascript_chrome/webpack.config.js
@@ -1,21 +1,26 @@
 const path = require('path');
 
 let exclude = [path.resolve(__dirname, "html")];
-console.log(exclude);
 
 module.exports = {
+	mode: "development",
 	devtool: "source-map",
-	output: {
-		path: path.resolve(__dirname, "html", "js"),
-		filename: "webpack.js",
-	},
 	resolve: {
 		extensions: [".ts", ".js"]
 	},
 	module: {
 		rules: [
-			{ test: /\.ts$/, loader: "awesome-typescript-loader", exclude },
-			{ enforce: "pre", test: /\.js$/, loader: "source-map-loader", exclude }
+			{
+				test: /\.ts$/,
+				loader: "awesome-typescript-loader",
+				exclude
+			},
+			{
+				enforce: "pre",
+				test: /\.js$/,
+				loader: "source-map-loader",
+				exclude
+			}
 		]
 	},
 	devServer: {
